a8dda63de9e85f7c0af8966c2f88f4f0dd8350ab
MARWANANO32 <marwanano32@gmail.com>
2024-10-07 14:40:57 +0300

    make the menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
+import Menu from "@/components/layout/menu";
 
 export default function Home() {
   return (
     <>
     <Header />
     <Hero />
+    <Menu />
     </>
   );
 }
---
+++ src/components/layout/menu.tsx
@@ -0,0 +1,8 @@
+export default function Menu() {
+    return (
+        <div>
+            <h3>Check out</h3>
+            <h2>Menu</h2>
+        </div>
+    );
+}
