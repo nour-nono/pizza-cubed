e09766e9834a0f994d26cf07a7d3850cf7fbe316
nono <nour98nour@gmail.com>
2024-10-09 00:40:12 +0300

    Refactor registration page to handle user creation and display success/error messages

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,30 +1,50 @@
 "use client";
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 
 const RegisterPage = () => {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
-  function handleSubmit(ev: React.FormEvent) {
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
+  async function handleSubmit(ev: React.FormEvent) {
     ev.preventDefault();
-    fetch("/api/register", {
+    setCreatingUser(true);
+    setUserCreated(false);
+    setError(false);
+    const response = await fetch("/api/register", {
       method: "POST",
+      body: JSON.stringify({ userEmail, userPassword }),
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: userEmail,
-        password: userPassword,
-      }),
     });
+    response.ok ? setUserCreated(true) : setError(true);
+    setCreatingUser(false);
   }
   return (
     <section className="mt-8">
       <h1 className="text-center text-primary text-4xl mb-4">Register</h1>
       <form className="max-w-xs mx-auto" onSubmit={handleSubmit}>
+      {userCreated && (
+        <div className="my-4 text-center">
+          User created.<br />
+          Now you can 
+          <Link className="underline" href={'/login'}>Login &raquo;</Link>
+        </div>
+      )}
+      {error && (
+        <div className="my-4 text-center">
+          An error has occurred.<br />
+          Please try again later
+        </div>
+      )}
         <input
           type="email"
           placeholder="email"
           value={userEmail}
           required
+          disabled={creatingUser}
           onChange={(ev) => {
             setUserEmail(ev.target.value);
           }}
@@ -34,11 +54,12 @@ const RegisterPage = () => {
           placeholder="password"
           value={userPassword}
           required
+          disabled={creatingUser}
           onChange={(ev) => {
             setUserPassword(ev.target.value);
           }}
         />
-        <button type="submit">Register</button>
+        <button type="submit" disabled={creatingUser}>Register</button>
         <div className="my-4 text-center text-gray-500">
           or login with provider
         </div>
@@ -46,6 +67,10 @@ const RegisterPage = () => {
           <Image src={"/google.png"} alt={""} width={24} height={24} />
           Login with google
         </button>
+        <div className="text-center my-4 text-gray-500 border-t pt-4">
+          Existing account? 
+          <Link className="underline" href={'/login'}>Login here &raquo;</Link>
+        </div>
       </form>
     </section>
   );
