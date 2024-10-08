5e7c3dcefbba9c1cc4c03ffef44204bcbb95600d
nono <nour98nour@gmail.com>
2024-10-08 20:59:48 +0300

    Add bcrypt, mongodb, and mongoose dependencies

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,3 +1,10 @@
-export function POST(req: Request) {
-  return Response.json({ message: "Hello from the API" });
+import { User } from "@/app/models/User";
+import mongoose from "mongoose";
+export async function POST(req: Request) {
+  const body = await req.json();  // Parse the request body it should be {email: string, password: string }
+  mongoose.connect(process.env.MONGODB_URI as string);  // Connect to the database
+  mongoose.set("strictQuery", true);  // if true, mongoose will throw an error if you try to query by a field that is not in the schema
+  const user = new User(body);  // Create a new user
+  await user.save();  // Save the user to the database
+  return Response.json(user);
 }
