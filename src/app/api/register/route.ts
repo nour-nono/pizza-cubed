5250a2f956403816617a5a64eaa9ad1d67faceb1
nono <nour98nour@gmail.com>
2024-10-09 01:37:41 +0300

    Refactor database connection in registration route

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export async function POST(req: Request) {
   const body = await req.json();  // Parse the request body it should be {email: string, password: string }
   mongoose.connect(process.env.MONGODB_URI as string,
-    {dbName: "pizza_cubed"}
+    {dbName: process.env.MONGODB_DB as string}
   );  // Connect to the database
   mongoose.set("strictQuery", true);  // if true, mongoose will throw an error if you try to query by a field that is not in the schema
   const user = new User(body);  // Create a new user
