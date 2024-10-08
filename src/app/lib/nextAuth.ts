2cdff36b87411b1911eb0fe8d6b128c5a3c81cc7
nono <nour98nour@gmail.com>
2024-10-08 20:03:58 +0300

    Refactor NextAuth route handler and import nextAuth options from lib/nextAuth

--- src/app/lib/nextAuth.ts
+++ src/app/lib/nextAuth.ts
@@ -4,39 +4,31 @@ import CredentialsProvider from "next-auth/providers/credentials";
 export const nextAuth: AuthOptions = {
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
+      name: 'Credentials',
+      id: 'credentials',
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" }
+        username: { label: "Email", type: "email", placeholder: "test@example.com" },
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
         // Add logic here to look up the user from the credentials supplied
         const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-  
         if (user) {
-          // Any object returned will be saved in `user` property of the JWT
+        //   // Any object returned will be saved in `user` property of the JWT
           return user
         } else {
-          // If you return null then an error will be displayed advising the user to check their details.
+        //   // If you return null then an error will be displayed advising the user to check their details.
           return null
-  
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+
+        //   // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
         }
       }
-    })
-  ]
+    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    // }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
-// providers: [
-//   GoogleProvider({
-//     clientId: process.env.GOOGLE_CLIENT_ID as string,
-//     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-//   }),
-// ],
-
-// secret: process.env.NEXTAUTH_SECRET,
