4826733ef3b6be9b81f670177b706b67dddba4c7
mamdouhelnahla75 <100003950+mamdouhelnahla75@users.noreply.github.com>
2024-10-13 15:10:28 +0300

    update-user-schema

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -2,6 +2,7 @@ import { model, models, Schema } from "mongoose";
 import bcrypt from "bcrypt";
 
 const UserSchema = new Schema({
+  name: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String },
 }, { timestamps: true });
