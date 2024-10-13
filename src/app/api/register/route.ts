c43d85b071028a4d668902fe631f4d8c7e6e5845
mamdouhelnahla75 <100003950+mamdouhelnahla75@users.noreply.github.com>
2024-10-13 15:44:59 +0300

    pull-feat/auth&merge

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,12 +1,50 @@
 import { User } from "@/app/models/User";
 import mongoose from "mongoose";
+import { z } from "zod";
+
 export async function POST(req: Request) {
-  const body = await req.json();  // Parse the request body it should be {email: string, password: string }
-  mongoose.connect(process.env.MONGODB_URI as string,
-    {dbName: process.env.MONGODB_DB as string}
-  );  // Connect to the database
-  mongoose.set("strictQuery", true);  // if true, mongoose will throw an error if you try to query by a field that is not in the schema
-  const user = new User(body);  // Create a new user
-  await user.save();  // Save the user to the database
-  return Response.json(user);
+  const body = await req.json();
+
+  const userSchema = z
+    .object({
+      email: z.string().email("Incorrect Email"),
+      password: z.string().min(8, "Password need to be more than 8 carachtre"),
+      confirmPassword: z
+        .string()
+        .min(8, "Password need to be more than 8 carachtre"),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: "Passwords don't match",
+      path: ["confirmPassword"],
+    });
+
+  try {
+    userSchema.parse(body);
+
+    await mongoose.connect(`mongodb://${process.env.MONGODB_URI}/myapp`, {
+      dbName: process.env.MONGODB_DB as string,
+    });
+
+    const { email, password } = body;
+    const existedUser = await User.findOne({ email });
+
+    if (existedUser) {
+      throw {
+        errors: [{ message: "User with provided email already exists" }],
+        status: 400,
+      };
+    }
+
+    const user = new User({ email, password });
+    await user.save();
+    return Response.json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    console.log(">>", error);
+    const { errors, status } = error as { errors: any; status?: number };
+    return Response.json({ error: errors }, { status: status || 500 });
+  } finally {
+    mongoose.disconnect();
+  }
 }
