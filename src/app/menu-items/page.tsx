8d7d00a677944aee2cdafd58ec4946b4f2a6f54f
MARWANANO32 <marwanano32@gmail.com>
2024-10-15 13:57:01 +0300

    Refactor MenuItemPage component and create MenuItemForm component

--- src/app/menu-items/page.tsx
+++ src/app/menu-items/page.tsx
@@ -1,27 +1,36 @@
 import UserTabs from "@/components/layout/UserTabs"
+import MenuItem from "@/components/menu/MenuItem"
+import Link from "next/link"
 
 
 export default function MenuItemPage() {
   return (
     <section className="mt-8">
       <UserTabs isAdmin={true}/>
-      <form className="mt-8 max-w-md mx-auto">
-        <div className="flex items-start gap-2">
-          <div className="grow">
-            <input type="text" />
-            <label>Item name</label>
-
-            <input type="text" />
-            <label>Description</label>
-
-            <input type="text" />
-            <label>Base Price</label>
-
-            <input type="text" />
-            <button type="submit">Save</button>
-          </div>
-        </div>
-      </form>
+      {/* <div className="mt-8">
+        <Link className="button flex" href={'/menu-items/new'}>
+          <span>Create new Menu Item</span>
+          <Right/>
+        <Link/>
+      </div> */}
+      <div>
+        <h2 className="text-sm text-gray-500 mt-8">Edit Menu item:</h2>
+        {/* <div className="grid grid-cols-3">
+          {menuItems?.length > 0 && menuItem.map(item => (
+            <Link href={'/menu-items/edit/'+item._id} 
+            className="bg-gray-300 rounded-lg p-4">
+              <div className="relative">
+                <Image className="rounded-md"
+                src={item.image} alt={''} width={100} height={100} />
+              </div>
+              <div className="text-center">
+                {item.name}
+              </div>
+              {item.name}
+            </Link>
+          ))}
+        </div> */}
+      </div>
     </section>
   )
 }
---
+++ src/components/layout/MenuItemForm.tsx
@@ -0,0 +1,5 @@
+export default function MenuItemForm() {
+  return (
+    
+  );
+}
