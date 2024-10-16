1635b9116d43f6f47c8c4ea38da3fbb026f93b04
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-10-16 23:40:55 +0100

    refacto: added input validation for categories routes

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,31 +1,101 @@
-import { isAdmin } from "@/app/api/auth/[...nextauth]/route";
-import { Category } from "@/app/models/Category";
-import mongoose from "mongoose";
+import { isAdmin } from '@/app/api/auth/[...nextauth]/route';
+import { Category } from '@/app/models/Category';
+import mongoose from 'mongoose';
+import { z } from 'zod';
 
 export async function POST(req: Request) {
+  if (!(await isAdmin())) {
+    return Response.json({ message: 'Access denied' }, { status: 403 });
+  }
+
+  const body = await req.json();
+  const createCategorySchema = z.object({
+    name: z
+      .string({ message: 'Category name should be a string' })
+      .min(1, 'Category name is too short'),
+  });
+  const validationResult = createCategorySchema.safeParse(body);
+
+  if (!validationResult.success) {
+    return Response.json({ error: validationResult.error.issues });
+  }
+
   if (!process.env.MONGODB_URI) {
     throw new Error('Missing env variable: "MONGODB_URI"');
   }
   mongoose.connect(process.env.MONGODB_URI);
-  const { name } = await req.json();
-  if (await isAdmin()) {
-    const categoryDoc = await Category.create({ name });
-    return Response.json(categoryDoc);
-  } else {
-    return Response.json({});
-  }
+  const categoryDoc = await Category.create({
+    name: validationResult.data.name,
+  });
+  return Response.json(categoryDoc);
 }
 
 export async function PUT(req: Request) {
+  if (!(await isAdmin())) {
+    return Response.json({ message: 'Access denied' }, { status: 403 });
+  }
+
+  const body = await req.json();
+  const updateCategorySchema = z.object({
+    _id: z.string().refine((val) => {
+      return mongoose.Types.ObjectId.isValid(val);
+    }, 'Category id is incorrect'),
+    name: z
+      .string({ message: 'Category name should be a string' })
+      .min(1, 'Category name is too short'),
+  });
+  const validationResult = updateCategorySchema.safeParse(body);
+
+  if (!validationResult.success) {
+    return Response.json({ error: validationResult.error.issues });
+  }
+
   if (!process.env.MONGODB_URI) {
     throw new Error('Missing env variable: "MONGODB_URI"');
   }
   mongoose.connect(process.env.MONGODB_URI);
-  const { _id, name } = await req.json();
-  if (await isAdmin()) {
-    await Category.updateOne({ _id }, { name });
+  const { _id, name } = validationResult.data;
+  const result = await Category.updateOne({ _id }, { name });
+  if (result.modifiedCount === 0) {
+    return Response.json(
+      {
+        error: [
+          {
+            message: 'Category not found',
+          },
+        ],
+      },
+      { status: 404 },
+    );
   }
-  return Response.json(true);
+  return Response.json({ message: 'Category has been updated successfully' });
+}
+
+export async function DELETE(req: Request) {
+  if (!(await isAdmin())) {
+    return Response.json({ message: 'Access denied' }, { status: 403 });
+  }
+
+  if (!process.env.MONGODB_URI) {
+    throw new Error('Missing env variable: "MONGODB_URI"');
+  }
+  mongoose.connect(process.env.MONGODB_URI);
+  const url = new URL(req.url);
+  const _id = url.searchParams.get('_id');
+
+  const updateCategorySchema = z.object({
+    _id: z.string().refine((val) => {
+      return mongoose.Types.ObjectId.isValid(val);
+    }, 'Category id is incorrect'),
+  });
+  const validationResult = updateCategorySchema.safeParse({ _id });
+
+  if (!validationResult.success) {
+    return Response.json({ error: validationResult.error.issues });
+  }
+
+  await Category.deleteOne({ _id });
+  return Response.json({ message: 'Category has been deleted successfully' });
 }
 
 export async function GET() {
@@ -33,18 +103,6 @@ export async function GET() {
     throw new Error('Missing env variable: "MONGODB_URI"');
   }
   mongoose.connect(process.env.MONGODB_URI);
-  return Response.json(await Category.find());
-}
-
-export async function DELETE(req: Request) {
-  if (!process.env.MONGODB_URI) {
-    throw new Error('Missing env variable: "MONGODB_URI"');
-  }
-  mongoose.connect(process.env.MONGODB_URI);
-  const url = new URL(req.url);
-  const _id = url.searchParams.get("_id");
-  if (await isAdmin()) {
-    await Category.deleteOne({ _id });
-  }
-  return Response.json(true);
+  const categories = await Category.find();
+  return Response.json(categories);
 }
