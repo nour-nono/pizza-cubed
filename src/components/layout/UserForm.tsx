921a588995e58dc69580d93b0780166ca7074038
nono <nour98nour@gmail.com>
2024-10-19 12:15:44 +0300

    Refactor package.json and package-lock.json to add Cloudinary configuration

--- src/components/layout/UserForm.tsx
+++ src/components/layout/UserForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 import AddressInputs from '@/components/layout/AddressInputs';
 import { useState } from 'react';
+import { CldUploadWidget, CldImage } from 'next-cloudinary';
 
 export default function UserForm({ user, onSave }) {
   const [userName, setUserName] = useState(user?.name || '');
@@ -35,6 +36,46 @@ export default function UserForm({ user, onSave }) {
           })
         }
       >
+        {/* <CldImage
+          src='vluxuh1d58bd3g3tcowe' // Use this sample image or upload your own via the Media Explorer
+          width='500' // Transform the image: auto-crop to square aspect_ratio
+          height='500'
+          alt='Profile Image'
+          crop={{
+            type: 'auto',
+            source: true,
+          }}
+        />
+        <CldUploadWidget
+          signatureEndpoint='/api/sign-image'
+          onSuccess={async (results) => {
+            console.log("public_id", results.info.public_id);
+            console.log("email", user);
+            const res = await fetch('/api/upload', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                image: results.info.public_id,
+                email: user?.email,
+              }),
+            });
+            console.log(res);
+          }}
+          options={{ sources: ['local'] }}
+        >
+          {({ open }) => {
+            return (
+              <button
+                type='button'
+                onClick={() => open()}
+              >
+                Upload an Image
+              </button>
+            );
+          }}
+        </CldUploadWidget> */}
         <label>First and last name</label>
         <input
           type='text'
