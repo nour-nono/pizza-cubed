3776553986d0bce19285116f7d977bb7ef64b20d
fati zid <97165021+fatimaZohraEzzaidani@users.noreply.github.com>
2024-10-22 00:57:59 +0100

    feat: get all orders for admin and user, and cancel order

---
+++ src/app/api/orders/route.ts
@@ -0,0 +1,44 @@
+import { isAdmin, getUserEmail } from '@/app/api/auth/[...nextauth]/route';
+import { MongoDBConnection } from '@/app/lib/mongoClient';
+import { Order } from '@/app/models/Order';
+import mongoose from 'mongoose';
+import { z } from 'zod';
+
+export async function GET() {
+  await MongoDBConnection();
+  let options = {};
+
+  if (!(await isAdmin())) {
+    const email = await getUserEmail();
+    options = {
+      userEmail: email,
+    };
+  }
+  const orders = await Order.find(options);
+  if (!orders.length) {
+    return Response.json(
+      { error: [{ message: 'No orders founds' }] },
+      { status: 404 },
+    );
+  }
+  return Response.json(orders);
+}
+
+export async function PUT(req: Request) {
+  const url = new URL(req.url);
+  const _id = url.searchParams.get('_id');
+
+  const updateOrderSchema = z.object({
+    _id: z.string().refine((val) => {
+      return mongoose.Types.ObjectId.isValid(val);
+    }, 'Order id is incorrect'),
+  });
+  const validationResult = updateOrderSchema.safeParse({ _id });
+
+  if (!validationResult.success) {
+    return Response.json({ error: validationResult.error.issues });
+  }
+
+  await Order.updateOne({ _id }, { canceled: true });
+  return Response.json({ message: 'Order has been canceled successfully' });
+}
